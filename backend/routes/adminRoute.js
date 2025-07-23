d4880db9d0e2ec7dda174f0fe1e6f9901bbdf6ec
abdulrahmanmohd297 <abdulrahmanmohd297@gmail.com>
2025-07-22 21:44:23 -0500

    Admin Role of adding doctors

---
+++ backend/routes/adminRoute.js
@@ -0,0 +1,10 @@
+import { addDoctor } from "../controllers/adminController.js";
+import express from "express";
+import upload from "../middlewares/multer.js";
+
+const adminRouter = express.Router();
+
+
+adminRouter.post("/add-doctor", upload.single("image"), addDoctor);
+
+export default adminRouter;
