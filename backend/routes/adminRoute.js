e7cbf557e72a13df7d53f43e6ab115c0c10d1b71
abdulrahmanmohd297 <abdulrahmanmohd297@gmail.com>
2025-07-22 21:59:33 -0500

    Admin Login functionality

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,10 +1,13 @@
-import { addDoctor } from "../controllers/adminController.js";
+import { addDoctor, loginAdmin } from "../controllers/adminController.js";
 import express from "express";
 import upload from "../middlewares/multer.js";
+import authAdmin from "../middlewares/authAdmin.js";
 
 const adminRouter = express.Router();
 
 
-adminRouter.post("/add-doctor", upload.single("image"), addDoctor);
+adminRouter.post("/add-doctor", authAdmin, upload.single("image"), addDoctor);
+adminRouter.post("/login", loginAdmin);
+
 
 export default adminRouter;
