c850c93f2ff582d0eff4863a9609aa455d819be4
abdulrahmanmohd297 <abdulrahmanmohd297@gmail.com>
2025-08-06 11:28:30 -0500

    listing appointments of a user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -196,4 +196,25 @@ const bookAppointment = async (req, res) => {
 };
 
 
-export { registerUser, loginUser, getProfile, updateProfile, bookAppointment };
+// API to get user appointments for frontend my-appointments page
+const listAppointment = async (req, res) => {
+  try {
+    const  userId  = req.userId; // using req.userId set by authUser middleware
+    const appointments = await appointmentModel.find({ userId });
+
+    res.json({ success: true, appointments });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+
+export {
+  registerUser,
+  loginUser,
+  getProfile,
+  updateProfile,
+  bookAppointment,
+  listAppointment,
+};
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -4,7 +4,8 @@ import {
   loginUser,
   getProfile,
   updateProfile,
-  bookAppointment
+  bookAppointment,
+  listAppointment
 } from "../controllers/userController.js";
 import authUser from "../middlewares/authUser.js";
 import upload from '../middlewares/multer.js';
@@ -18,7 +19,7 @@ userRouter.post("/login", loginUser);
 userRouter.get("/get-profile", authUser, getProfile);
 userRouter.post('/update-profile',upload.single('image'),authUser,updateProfile);
 userRouter.post("/book-appointment", authUser, bookAppointment);
-
+userRouter.get("/appointments", authUser, listAppointment);
 
 
 
